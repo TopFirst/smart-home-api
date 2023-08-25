a694f56c4457b66fce67e315896034dc419b1c0a
Topik Topik <topfirst4@gmail.com>
2023-08-26 00:15:31 +0700

    add home message

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to Simple Smart Home API!');
+});
 app.get('/api/test',(req,res)=>{
     res.send('yeay! berhasil');
 });
